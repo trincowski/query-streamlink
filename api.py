3798cef1d702586ed68e9dbd8b306bae8f786f19
Ismaël Moret <30985701+BellezaEmporium@users.noreply.github.com>
2024-07-02 16:31:41 +0200

    Simplify API.

--- api.py
+++ api.py
@@ -3,32 +3,33 @@ from streamlink import NoPluginError, PluginError
 from streamlink.stream import DASHStream
 import urllib.request
 
-
 def get_streams(query, quality="best"):
     try:
-        streams = streamlink.streams(query).items()
+        streams = streamlink.streams(query)
         if not streams:
             return "No streams found."
-        for stream_quality, link in streams:
-            if quality is not None and quality != stream_quality:
+        
+        for stream_quality, link in streams.items():
+            if quality and quality != stream_quality:
                 continue
+
+            url = link.to_url()
             if 'dailymotion.com' in query or 'dai.ly' in query:
-                url = link.to_manifest_url()
-                if 'https://www.dailymotion.com/cdn/live/video/' in url:
-                    response = urllib.request.urlopen(url)
+                manifest_url = link.to_manifest_url()
+                if 'https://www.dailymotion.com/cdn/live/video/' in manifest_url:
+                    response = urllib.request.urlopen(manifest_url)
                     data = response.read().decode('utf-8')
                     replacements = {'live-3': 'live-0', 'live-2': 'live-0', 'live-1': 'live-0'}
-                    for key in replacements:
+                    for key, replacement in replacements.items():
                         if key in data:
-                            l0_link = link.to_url()
-                            l0_replace = l0_link.replace('live-0', replacements[key])
-                            return l0_replace
-                    return link.to_url()
-                else:
+                            return url.replace('live-0', replacement)
                     return url
-            if isinstance(link, DASHStream) or ("best" in stream_quality and "chunklist" in link.to_url()) or "live" in stream_quality or "http" in stream_quality:
-                return link.to_url()
-        else:
-            return link.to_manifest_url()
+                return manifest_url
+            
+            if isinstance(link, DASHStream) or any(term in stream_quality for term in ["best", "live", "http"]) or "chunklist" in url:
+                return url
+
+        return link.to_manifest_url()
+
     except (ValueError, NoPluginError, PluginError, Exception) as ex:
         return f"An error occurred: {ex}"
